c03ea9c9a9bf9c6b15647be721f63d8436984631
Emily Lu <emily.lu@ou.edu>
2019-05-02 01:09:28 -0500

    debugged version

--- Desktop/project5/Project FIVE s2/src/HammingDist.java
+++ Desktop/project5/Project FIVE s2/src/HammingDist.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -16,10 +18,10 @@ import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class HammingDist extends JFrame{
-
-	
 	/**
 	 * The only panel that will be used 
 	 */
@@ -83,59 +85,106 @@ public class HammingDist extends JFrame{
 	 * TextBox color
 	 */
 	private Color color = new Color(50,150,230);
-
+	
 	public HammingDist() throws IOException{
 		
-		setFrame();		// setting initial frame
-		
-		setSlider();	// setting slider
-		
-		setTextFields();	// setting text fields
-		
-		setComboBox();		// setting combo box
-		
-		setButtons();	// setting up buttons
-		
-		// setting initial frame
-	}
+	setFrame();		// setting initial frame
 	
+	setSlider();	// setting slider
+	
+	setTextFields();	// setting text fields
+	
+	setComboBox();		// setting combo box
+	
+	setButtons();	// setting up buttons
+	
+	// adding all the buttons to the panel
+	panel1.add(showStation);
+	panel1.add(calcHD);
+	panel1.add(addStat);
+	panel1.add(input);
+	panel1.add(slider);
+	panel1.add(stationList);
+	
+	// adding all the texts to the panel
+	panel1.add(text0);
+	panel1.add(text1);
+	panel1.add(text2);
+	panel1.add(text3);
+	panel1.add(text4);
+	panel1.add(sliderText);
+	panel1.add(compareText);
+	
+	// adding all the text boxes to the panel
+	panel1.add(dist0);
+	panel1.add(dist1);
+	panel1.add(dist2);
+	panel1.add(dist3);
+	panel1.add(dist4);
+	
+	panel1.add(sliderValue);
+	panel1.add(showLabel);
+	
+	// adding the panel to the frame
+	this.add(panel1);
+	}
 	
 	private void setTextFields() {
-		// default text
-		input.setText("ZERO");
-		
-		// setting bounds for each text and text boxes
-		input.setBounds(150,700,100,25);
-		
-		sliderText.setBounds(15,8,120,15);
-		compareText.setBounds(20,415,120,15);
-		
-		dist0.setBorder(BorderFactory.createLineBorder(color, 1));
-		dist0.setBounds(150,495,100,20);
-		
-		dist1.setBorder(BorderFactory.createLineBorder(color, 1));
-		dist1.setBounds(150,535,100,20);
-		
-		dist2.setBorder(BorderFactory.createLineBorder(color, 1));
-		dist2.setBounds(150,585,100,20);
-		
-		dist3.setBorder(BorderFactory.createLineBorder(color, 1));
-		dist3.setBounds(150,625,100,20);
-		
-		dist4.setBorder(BorderFactory.createLineBorder(color, 1));
-		dist4.setBounds(150,660,100,20);
+	// default text
+	input.setText("ZERO");
+	
+	// setting bounds for each text and text boxes
+	input.setBounds(150,700,100,25);
+	
+	sliderText.setBounds(15,8,120,15);
+	compareText.setBounds(20,415,120,15);
+	
+	dist0.setBorder(BorderFactory.createLineBorder(color, 1));
+	dist0.setBounds(150,495,100,20);
+	
+	dist1.setBorder(BorderFactory.createLineBorder(color, 1));
+	dist1.setBounds(150,535,100,20);
+	
+	dist2.setBorder(BorderFactory.createLineBorder(color, 1));
+	dist2.setBounds(150,585,100,20);
+	
+	dist3.setBorder(BorderFactory.createLineBorder(color, 1));
+	dist3.setBounds(150,625,100,20);
+	
+	dist4.setBorder(BorderFactory.createLineBorder(color, 1));
+	dist4.setBounds(150,660,100,20);
 
-		text0.setBounds(20,500,120,10);
-		text1.setBounds(20,545,120,10);
-		text2.setBounds(20,585,120,10);
-		text3.setBounds(20,625,120,10);
-		text4.setBounds(20,665,120,10);
-		
-		showLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
-		showLabel.setBackground(Color.WHITE);
-		showLabel.setOpaque(true);
-		showLabel.setBounds(25,150,200,250);
+	text0.setBounds(20,500,120,10);
+	text1.setBounds(20,545,120,10);
+	text2.setBounds(20,585,120,10);
+	text3.setBounds(20,625,120,10);
+	text4.setBounds(20,665,120,10);
+	
+	showLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+	showLabel.setBackground(Color.WHITE);
+	showLabel.setOpaque(true);
+	showLabel.setBounds(25,150,200,250);
+	
 	}
+	private void setSlider() { 
+		slider.setBounds(10,25,200,50);		// setting the bound, spaces and color for the slider
+		slider.setPaintTicks(true);
+		slider.setMajorTickSpacing(1);
+		slider.setPaintLabels(true);
+		
+		sliderValue.setBorder(BorderFactory.createLineBorder(color, 1));		// setting up the slider value display
+		sliderValue.setBounds(140, 5, 100, 20);
+		sliderValue.setText(" " + slider.getValue());
+		
+		slider.addChangeListener(new ChangeListener() {			// adding action listener to the slider 
+			@Override
+			public void stateChanged(ChangeEvent e) {
+			sliderValue.setText(" " + slider.getValue());		// setting slider Value to the chosen value
+		}
+		
+	});
+	}
+	
 	
 	private void setButtons() {
 		// setting up the buttons
@@ -272,52 +321,42 @@ public class HammingDist extends JFrame{
 	
 	
 	private void setComboBox() throws IOException {
-		
-		// reading in the data from the text file
-		BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
-		
-		// loading the file into the stations list
-		List<String> stations = new ArrayList<String>();
-		try {
-			String line = null;
-			while((line = br.readLine()) != null) {
-				stations.add(line);
-				
-			}
-		}
-		catch(FileNotFoundException e) {	// print error message if the file is not found
-			System.err.println("Error, file Mesonet.txt didn't exist.");
-		}
-		finally {
-			br.close();
-		}
-		
-		// setting bound for combo box, and loading stations into combo box
-		String[] lines = stations.toArray(new String[] {});
-		stationList = new JComboBox<>(lines);
-		stationList.setBounds(145,410,70,20);
-		}
 	
-	private void setSlider() { 
-		slider.setBounds(10,25,200,50);		// setting the bound, spaces and color for the slider
-		slider.setPaintTicks(true);
-		slider.setMajorTickSpacing(1);
-		slider.setPaintLabels(true);
-		
-		sliderValue.setBorder(BorderFactory.createLineBorder(color, 1));		// setting up the slider value display
-		sliderValue.setBounds(140, 5, 100, 20);
-		sliderValue.setText(" " + slider.getValue());
+	// reading in the data from the text file
+	BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
+	
+	// loading the file into the stations list
+	List<String> stations = new ArrayList<String>();
+	try {
+		String line = null;
+		while((line = br.readLine()) != null) {
+			stations.add(line);
+			
+		}
+	}
+	catch(FileNotFoundException e) {	// print error message if the file is not found
+		System.err.println("Error, file Mesonet.txt didn't exist.");
+	}
+	finally {
+		br.close();
 	}
 	
-		private void setFrame() {
-		setTitle("Hamming Distance");
-		setSize(600,800);
-		JPanel panel1 = new JPanel(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		
-		}
-		
+	// setting bound for combo box, and loading stations into combo box
+	String[] lines = stations.toArray(new String[] {});
+	stationList = new JComboBox<>(lines);
+	stationList.setBounds(145,410,70,20);
+	}
+	
+	// setting initial frame
+	private void setFrame() {
+	setTitle("Hamming Distance");
+	setSize(400,800);
+	panel1 = new JPanel(null);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setVisible(true);
+	
+	}
+	
 	public static void main(String[] args) throws IOException{
 		HammingDist HD = new HammingDist();
 		HD.revalidate();
