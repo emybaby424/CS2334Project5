c076e9281577bfd1b66b531cefe7dcb75e733f59
Emily Lu <emily.lu@ou.edu>
2019-05-02 00:33:14 -0500

    init Frame added

--- Desktop/project5/Project FIVE s2/src/HammingDist.java
+++ Desktop/project5/Project FIVE s2/src/HammingDist.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class HammingDist extends JFrame{
 
@@ -9,13 +10,25 @@ public class HammingDist extends JFrame{
 		
 		setFrame();		// setting initial frame
 		
-		setSlider();	// setting slider
+		//setSlider();	// setting slider
 		
-		setTextFields();	// setting text fields
+		//setTextFields();	// setting text fields
 		
-		setComboBox();		// setting combo box
+		//setComboBox();		// setting combo box
 		
-		setButtons();	// setting up buttons
+		//setButtons();	// setting up buttons
+		
+		// setting initial frame
+	}
+	
+		private void setFrame() {
+		setTitle("Hamming Distance");
+		setSize(600,800);
+		JPanel panel1 = new JPanel(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		}
 		
 	public static void main(String[] args) throws IOException{
 		HammingDist HD = new HammingDist();
