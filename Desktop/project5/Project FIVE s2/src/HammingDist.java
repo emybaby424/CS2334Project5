7f40d6ee3ddc175f4e9078bbeef4c7b34d9bbd25
Emily Lu <emily.lu@ou.edu>
2019-05-02 00:56:39 -0500

    Variables added

--- Desktop/project5/Project FIVE s2/src/HammingDist.java
+++ Desktop/project5/Project FIVE s2/src/HammingDist.java
@@ -2,22 +2,82 @@ import java.awt.Color;
 import java.io.IOException;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class HammingDist extends JFrame{
 
 	
+	/**
+	 * The only panel that will be used 
+	 */
+	private JPanel panel1;
+	
+	/**
+	 * A button that will perform the action of showing stations
+	 * A button that will calculate Hamming Distance
+	 * A button that will add stations
+	 */
+	private JButton showStation = new JButton("Show Station");
+	private JButton calcHD = new JButton("Calculate HD");
+	private JButton addStat = new JButton("Add Station");
+	
+	/**
+	 * TextArea will be displayed inside a JScrollPane
+	 */
+	private JTextArea intext = new JTextArea(24, 12);
+	private JScrollPane showLabel = new JScrollPane(intext);
+	
+	/**
+	 * A slider that will decide the Hamming Distance for showing
+	 */
 	private JSlider slider = new JSlider(1,4);
+	
+	/**
+	 * TextField where user will input stations
+	 */
+	private JTextField input = new JTextField(10);
+	/**
+	 * A drop box with all the stations stored in it
+	 */
+	private JComboBox<String> stationList;
 	
 	/**
 	 * slider value
 	 */
 	private JLabel sliderValue = new JLabel();
-
-	private Color color;
+	
+	/**
+	 * distance texts
+	 */
+	private JLabel text0 = new JLabel("Distance 0");
+	private JLabel text1 = new JLabel("Distance 1");
+	private JLabel text2 = new JLabel("Distance 2");
+	private JLabel text3 = new JLabel("Distance 3");
+	private JLabel text4 = new JLabel("Distance 4");
+	private JLabel sliderText = new JLabel("Enter Hamming Dist:");
+	private JLabel compareText = new JLabel("Compare with");
+	
+	/**
+	 * Number of stations will the same distance value
+	 */
+	private JLabel dist0 = new JLabel();
+	private JLabel dist1 = new JLabel();
+	private JLabel dist2 = new JLabel();
+	private JLabel dist3 = new JLabel();
+	private JLabel dist4 = new JLabel();
+	
+	/**
+	 * TextBox color
+	 */
+	private Color color = new Color(50,150,230);
 
 	public HammingDist() throws IOException{
 		
@@ -43,19 +103,19 @@ public class HammingDist extends JFrame{
 		sliderText.setBounds(15,8,120,15);
 		compareText.setBounds(20,415,120,15);
 		
-		dist0.setBorder(BorderFactory.createLineBorder(only, 1));
+		dist0.setBorder(BorderFactory.createLineBorder(color, 1));
 		dist0.setBounds(150,495,100,20);
 		
-		dist1.setBorder(BorderFactory.createLineBorder(only, 1));
+		dist1.setBorder(BorderFactory.createLineBorder(color, 1));
 		dist1.setBounds(150,535,100,20);
 		
-		dist2.setBorder(BorderFactory.createLineBorder(only, 1));
+		dist2.setBorder(BorderFactory.createLineBorder(color, 1));
 		dist2.setBounds(150,585,100,20);
 		
-		dist3.setBorder(BorderFactory.createLineBorder(only, 1));
+		dist3.setBorder(BorderFactory.createLineBorder(color, 1));
 		dist3.setBounds(150,625,100,20);
 		
-		dist4.setBorder(BorderFactory.createLineBorder(only, 1));
+		dist4.setBorder(BorderFactory.createLineBorder(color, 1));
 		dist4.setBounds(150,660,100,20);
 
 		text0.setBounds(20,500,120,10);
@@ -68,6 +128,7 @@ public class HammingDist extends JFrame{
 		showLabel.setBackground(Color.WHITE);
 		showLabel.setOpaque(true);
 		showLabel.setBounds(25,150,200,250);
+	}
 	
 	private void setSlider() { 
 		slider.setBounds(10,25,200,50);		// setting the bound, spaces and color for the slider
